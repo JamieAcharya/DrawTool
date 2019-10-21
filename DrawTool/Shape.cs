bc669172543ce2e03298ecd25bab6afd75ffcec9
JamieA <git@jamieacharya.com>
2019-10-21 22:59:24 +0100

    Created abstract class 'Shape'

---
+++ DrawTool/Shape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawTool
+{
+    public abstract class Shape //Abstract Class
+    {
+        //Abstract method
+        protected int x, y;
+        protected int size = 10;
+        protected Pen myPen = new Pen(Color.Black);
+        public abstract void Display(Graphics drawArea);
+    }
+}
